feb3c5a37b064af2802f5019b5ddab2afe56c7b7
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-07-18 00:40:45 +0600

    local storage data store done

--- src/feature/noteSlice/noteSlice.js
+++ src/feature/noteSlice/noteSlice.js
@@ -4,12 +4,19 @@ let initialState = {
   noteItem: [],
 };
 
+let saveNote = JSON.parse(localStorage.getItem("note"));
+if (saveNote) {
+  initialState.noteItem = saveNote;
+}
 export const noteSlice = createSlice({
   name: "note",
   initialState,
+
   reducers: {
     addNote: (state, action) => {
+      //   state.noteItem.push(action.payload);
       state.noteItem = [...state.noteItem, action.payload];
+      localStorage.setItem("note", JSON.stringify(state.noteItem));
     },
   },
 });
